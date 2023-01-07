d7fbf42c98d4bf90464cf1a9d3f1f2e0e71add0f
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 22:53:34 +0300

    Add files via upload

---
+++ 08ClasslarProperty/Musteri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08ClasslarProperty
+{
+    internal class Musteri
+    {
+        public int Id { get; set; } // prop yazıp 2 kez taba basarsak müşterinin özelliklerini kullanmak için kullandığımız nesen oluşrur.
+
+        private string _adi;
+
+        public string Ad
+        {
+            get { return "Hoşgeldiniz: " + _adi; } // Değeri okurken başına Hoşgeldin ekletiyoruz.
+            set { _adi = value; } // 
+        }
+
+        public string Soyad;
+
+        public string Sehir { get; set; }
+    }
+}
---
+++ 08ClasslarProperty/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _08ClasslarProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "İlker";
+            musteri1.Soyad = "Ulaş";
+            musteri1.Sehir = "Tekirdağ";
+
+            Console.WriteLine(musteri1.Ad);
+
+        }
+    }
+}
