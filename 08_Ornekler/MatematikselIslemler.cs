8bd8de63fa4350aa3e948255d47591c552970d0d
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 22:51:21 +0300

    Add files via upload

---
+++ 08_Ornekler/MatematikselIslemler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Ornekler
+{
+    internal class MatematikselIslemler
+    {
+        public double KdvFiyatHesapla(double aratoplam, int kdvOran)
+        {
+            return (aratoplam * kdvOran) / 100;
+        }
+    }
+}
---
+++ 08_Ornekler/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08_Ornekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MatematikselIslemler mat = new MatematikselIslemler();
+
+            Urun urun1 = new Urun();
+
+            urun1.Id = 163487;
+            urun1.Adi = "Laptop";
+            urun1.Adet = 3;
+            urun1.KdvOran = 18;
+            urun1.Fiyat = 15638;
+            urun1.AraToplamFiyat = urun1.Fiyat * urun1.Adet;
+            urun1.KdvFiyati = mat.KdvFiyatHesapla(urun1.AraToplamFiyat, urun1.KdvOran);
+            urun1.GenelToplamFiyat = urun1.AraToplamFiyat + urun1.KdvFiyati;
+
+
+            Console.WriteLine("{0} Id'li {1} ürünün {2} adet {3} fiyattan satış işlemi sonrası {4} KDV oranı ile ara toplam {5}, KDV {6}, genel toplam {7} TL dir", urun1.Id, urun1.Adi, urun1.Adet, urun1.Fiyat, urun1.KdvOran, urun1.AraToplamFiyat, urun1.KdvFiyati, urun1.GenelToplamFiyat);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
