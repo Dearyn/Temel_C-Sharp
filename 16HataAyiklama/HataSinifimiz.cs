e877efcaa14ffb454f8fe5bfe3e2f4ac26fea4f5
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 23:10:56 +0300

    Add files via upload

---
+++ 16HataAyiklama/HataSinifimiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16HataAyiklama
+{
+    internal class HataSinifimiz : Exception
+    {
+        public HataSinifimiz(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ 16HataAyiklama/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _16HataAyiklama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				string[] ogrenciler = new string[3] { "İlker", "İlknur", "Firdevs" };
+				ogrenciler[3] = "Muhuttin";
+
+				Console.ReadLine();
+			}
+			catch (Exception hataBilgisi)
+			{
+				Console.WriteLine("Hata Var Sorun: {0}", hataBilgisi.Message);
+                Console.WriteLine("Hata Var Satır ve Konum Bilgisi: {0}", hataBilgisi.StackTrace);
+                Console.WriteLine("Hata Var Detay: {0}", hataBilgisi.InnerException);
+                Console.WriteLine("Hata Var YArdım Linki: {0}", hataBilgisi.HelpLink);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
