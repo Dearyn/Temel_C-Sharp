020e0a66ac3c5cc4521f2fc0f2fbf9a51e09a18d
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 23:01:31 +0300

    Add files via upload

---
+++ 12YapiciBloklar/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _12YapiciBloklar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriIslemleri musteriIslemleri = new MusteriIslemleri();
+
+            musteriIslemleri.Kaydet();
+
+            Console.ReadLine();
+
+            MusteriIslemleriTekrarla musteriIslemleriTekrarla = new MusteriIslemleriTekrarla(5);
+            musteriIslemleriTekrarla.Tekrarla();
+            musteriIslemleriTekrarla.EkranaYazdir();
+
+            Console.ReadLine();
+        }
+
+        class MusteriIslemleri
+        {
+            public MusteriIslemleri()
+            {
+                Console.WriteLine("Default Olarak Çalıştı");
+            }
+
+            public void Kaydet()
+            {
+                Console.WriteLine("Kaydetme İşlem Yapıldı");
+            }
+
+            public void Sil()
+            {
+                Console.WriteLine("Silme İşlemi Yapıldı");
+            }
+        }
+
+        class MusteriIslemleriTekrarla
+        {
+            private int _sayi = 10;
+
+            public MusteriIslemleriTekrarla(int sayi)
+            {
+                _sayi = sayi;
+            }
+
+            public MusteriIslemleriTekrarla()
+            {
+            }
+
+            public void Tekrarla()
+            {
+                Console.WriteLine("Yapı bloğu(Constructor) methoduyla newlendiği anda gönderilen sayi: {0}  Newlenmesinden sonra oluşaturulacak tüm işlemlerde bu sayı kullanılır." ,_sayi);
+            }
+
+            public void EkranaYazdir()
+            {
+                for (int i = 1; i <= _sayi; i++)
+                {
+                    Console.WriteLine("{0}-Merhaba", i);
+                }
+            }
+        }
+    }
+}
