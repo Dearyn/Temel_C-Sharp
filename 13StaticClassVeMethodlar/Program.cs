18c7053704091bd636c7ca4dda35b7431f70ac1f
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 23:06:27 +0300

    Add files via upload

---
+++ 13StaticClassVeMethodlar/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _13StaticClassVeMethodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Islemler islemler = new Islemler();
+            islemler.EkranaYaz();
+
+            ClassimNewlenmez.sayi = 5;
+            ClassimNewlenmez.sayi = 8;
+
+            Islemler.EkranaYazStatic();
+            Console.WriteLine(ClassimNewlenmez.sayi);
+            Console.ReadLine();
+
+            string cumle = "Merhaba arkadaşlar burada static olarak tanımlanmış AletCantam Classı içerisindeki static classlar ile işlem yapıyoruz";
+
+            string yeniCumle=AletCantam.BuyukHarfCevir(cumle);
+            yeniCumle=AletCantam.TurkceKarakterleriYokEt(yeniCumle);
+
+            Console.WriteLine(yeniCumle);
+
+            Console.ReadLine();
+        }
+    }
+
+    static class ClassimNewlenmez
+    {
+        public static int sayi { get; set; }
+    }
+
+    class Islemler
+    {
+        public void EkranaYaz()
+        {
+            Console.WriteLine("Merhaba Yazdım!");
+        }
+
+        public static void EkranaYazStatic()
+        {
+            Console.WriteLine("Merhaba Yazdım Static!");
+        }
+    }
+
+    static class StaticClassim
+    {
+        static StaticClassim()
+        {
+
+        }
+    }
+}
