9a81100a6d8364d78ec721d5e613f9ccd1215a21
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 22:57:37 +0300

    Add files via upload

---
+++ 10VirtualMethods/Program.cs
@@ -0,0 +1,47 @@
+﻿ namespace _10VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MysqlVeriTabani mysqlVeriTabani = new MysqlVeriTabani();
+            mysqlVeriTabani.VeriTabaninaKaydet();
+            mysqlVeriTabani.VeriTabanindanSil();
+
+            SqlServerVeriTabani sqlServerVeriTabani = new SqlServerVeriTabani();
+            sqlServerVeriTabani.VeriTabaninaKaydet();
+            sqlServerVeriTabani.VeriTabanindanSil();
+
+            Console.ReadLine();
+        }
+    }
+
+    class VeriTabaniIslemleri
+    {
+        public virtual void VeriTabaninaKaydet()
+        {
+            Console.WriteLine("Default olarak ana veritabanına eklendi");
+        }
+
+        public virtual void VeriTabanindanSil()
+        {
+            Console.WriteLine("Default olarak ana veritabanından silindi");
+        }
+    }
+
+    class MysqlVeriTabani : VeriTabaniIslemleri
+    {
+        public override void VeriTabaninaKaydet()
+        {
+            Console.WriteLine("MYSQL olarak ana veritabanına eklendi");
+        }
+    }
+
+    class SqlServerVeriTabani : VeriTabaniIslemleri
+    {
+        public override void VeriTabaninaKaydet()
+        {
+            Console.WriteLine("SQL olarak ana veritabanından eklendi");
+        }
+    }
+}
