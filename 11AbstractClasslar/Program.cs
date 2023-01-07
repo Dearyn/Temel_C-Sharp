5c8b0de1f4a79a4215433f21f1cb40dd5bed9370
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 22:58:52 +0300

    Add files via upload

---
+++ 11AbstractClasslar/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _11AbstractClasslar
+{
+    internal class Program
+    {
+        abstract class VeriTabani
+        {
+            public void Ekle()
+            {
+                Console.WriteLine("Ekleme Yapıldı");
+            }
+
+            public abstract void Silme();
+        }
+
+        class SqlVeriTabani : VeriTabani
+        {
+            public override void Silme()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class OracleVeriTabani : VeriTabani
+        {
+            public override void Silme()
+            {
+                Console.WriteLine("Oracle Silindi Yapıldı");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            VeriTabani veriTabani1 = new SqlVeriTabani();
+            veriTabani1.Ekle();
+            veriTabani1.Silme();
+
+            VeriTabani veriTabani2 = new OracleVeriTabani();
+            veriTabani2.Ekle();
+            veriTabani2.Silme();
+
+            Console.ReadLine();
+        }
+    }
+}
