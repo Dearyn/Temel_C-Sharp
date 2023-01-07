b71b490abb949e8803b81310484c043ff2e255c4
İlker Ulaş <113282021+Dearyn@users.noreply.github.com>
2023-01-07 23:04:47 +0300

    Add files via upload

---
+++ 12zOrnekler/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _12zOrnekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IslemYap islemYap = new IslemYap(5);
+            islemYap.EkranaYaz();
+
+            Console.ReadLine();
+        }
+
+    }
+    class IslemYap
+    {
+        private int _toplam = 0;
+        public IslemYap(int sayi1)
+        {
+            _toplam = sayi1;
+            Console.WriteLine("Gönderdiğiniz değer: {0}", _toplam);
+        }
+
+        public IslemYap(int sayi1, int sayi2)
+        {
+            _toplam = sayi1 + sayi2;
+            Console.WriteLine("{0} ve {1} sayısının toplamı: {2}", sayi1, sayi2, _toplam);
+        }
+
+        public IslemYap()
+        {
+            Console.WriteLine("Değer Gönderilmedi");
+        }
+
+        public void EkranaYaz()
+        {
+            for (int i = 1; i <= _toplam; i++)
+            {
+                Console.WriteLine("{0}-Merhaba", i);
+            }
+        }
+    }
+
+
+}
